de465dc8af1520e07f0c4e5729a52b29458fb7d1
Stanly Shiyanovskiy <st-small@rambler.ru>
2016-06-05 16:46:05 +0300

    no message

--- 45_Client_Server_APIs_T/45_Client_Server_APIs_T/SiSServerManager.m
+++ 45_Client_Server_APIs_T/45_Client_Server_APIs_T/SiSServerManager.m
@@ -77,7 +77,7 @@
                          
                          if (success) {
                              
-                             success(friendsArray);
+                             success(objectsArray);
                          }
                          
                          
